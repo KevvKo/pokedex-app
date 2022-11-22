699e1711feea1f66af423f06d5c80a9ff6aebef0
KevvKo <kklein915@gmail.com>
2022-11-22 10:58:37 +0100

    styling update for dialog

--- src/app/components/pokemon-detail/pokemon-detail.component.ts
+++ src/app/components/pokemon-detail/pokemon-detail.component.ts
@@ -11,6 +11,27 @@ export class PokemonDetailComponent {
 
   open: boolean = false;
   selectedPokemon: Pokemon;
+  typeColors: {[key: string]: string} = {
+    bug: 'bg-lime-500',
+    dark: 'bg-slate-900 text-slate-50',
+    dragon: 'bg-orange-400',
+    fairy: 'bg-emerald-500',
+    fighting: 'bg-orange-200',
+    fire: 'bg-orange-600',
+    flying: 'bg-red-200',
+    ghost: 'bg-zinc-400',
+    grass: 'bg-green-700 text-slate-50',
+    ground: 'bg-yellow-900 text-slate-50',
+    ice: 'bg-sky-400',
+    normal: 'bg-neutral-300',
+    rock: 'bg-stone-500',
+    poison: 'bg-purple-900 text-slate-50',
+    psychic: 'bg-violet-900 text-slate-50',
+    shadow: 'bg-slate-800 text-slate-50',
+    steel: 'bg-slate-400',
+    unknown: 'bg-neutral-800 text-slate-50',
+    water: 'bg-blue-600 text-slate-50'
+  }
 
   @Input() pokemon: Pokemon;
 
@@ -21,5 +42,9 @@ export class PokemonDetailComponent {
 
   toggleDialog(): void {
     this.open = !this.open
+  }
+
+  getClassByType(type: string): string {
+    return this.typeColors[type];
   }
 }
