f1e8d6ae548cf3e79fc22671773719d10af00b0f
KevvKo <kklein915@gmail.com>
2022-11-24 17:17:50 +0100

    small css update or card component

--- src/app/components/container/container.component.spec.ts
+++ src/app/components/container/container.component.spec.ts
@@ -1,10 +1,64 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ContainerComponent } from './container.component';
+import { Pokemon } from 'src/app/interfaces/interfaces';
 
 describe('ContainerComponent', () => {
   let component: ContainerComponent;
   let fixture: ComponentFixture<ContainerComponent>;
+
+  const mockPokemons: Pokemon[] = [
+    {
+      id: 1,
+      name: 'Testmon',
+      height: 123,
+      weight: 456,
+      stats: {
+        hp: 789,
+        attack: 0,
+        defense: 11,
+        specialAttack: 22,
+        specialDefense: 33,
+        speed: 44
+      },
+      types: ['flying', 'water'],
+      abilities: ['testing', 'debugging'],
+      sprite: '12345'
+    },
+    {
+      id: 2,
+      name: 'Testmon2',
+      height: 123,
+      weight: 456,
+      stats: {
+        hp: 789,
+        attack: 0,
+        defense: 11,
+        specialAttack: 22,
+        specialDefense: 33,
+        speed: 44
+      },
+      types: ['dragon'],
+      abilities: ['testing', 'debugging'],
+      sprite: '12345'
+    },
+    {
+      id: 3,
+      name: 'Testmon3',
+      height: 123,
+      weight: 456,
+      stats: {
+        hp: 789,
+        attack: 0,
+        defense: 11,
+        specialAttack: 22,
+        specialDefense: 33,
+        speed: 44
+      },
+      types: ['bug', 'fire'],
+      abilities: ['testing', 'debugging'],
+      sprite: '12345'
+    }  
+  ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
